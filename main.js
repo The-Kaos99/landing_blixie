14d1b8b1197eb4d800227a1caf83c712ce92db3f
Luzz00 <83696436+Luzz00@users.noreply.github.com>
2022-03-17 18:26:23 +0100

    roadmap detalles

--- main.js
+++ main.js
@@ -3,21 +3,35 @@
  let menuLH= document.querySelector(".menu_lateral_h");
  let menuLa=document.querySelector(".menu_lateral");
 
-
+let planet_cols= document.querySelectorAll(".planet_col");/*añadirles clase*/
 
  //por defecto, tiene que efectuarse una vez al cargarse la página. podría llamarlo justo al cargar
- if(window.innerWidth<575){
+ if(window.innerWidth<575){  //Para version movil
      //caja.innerHTML=menuLH;
      menuLH.style.display="inline";
      menuLa.style.display="none";
+
+     //a todas las columnas de planeta, añadir clase de boostrap
+    planet_cols.forEach(function(col){
+        col.classList.add("d-flex");
+        col.classList.add("justify-content-center");
+    });
+
  }
  else{
      menuLH.style.display="none";
      menuLa.style.display="block";
 
+      //a todas las columnas de planeta, quitar clase de boostrap
+      planet_cols.forEach(function(col){
+          col.classList.remove("justify-content-center");
+          col.classList.remove("d-flex");
+      });
+
  } 
  
- //__EVENTO al cambiar el ancho de la ventana. version movil y v grande
+ //__EVENTO al cambiar el ancho de la ventana. Para version movil y v.grande.
+ //En realidad con que se ejecute una sola vez al cargar el documento estaría correcto no
  window.addEventListener('resize', mensaje);
  
  function mensaje(){
@@ -25,7 +39,7 @@
     //console.log(window.innerWidth);
   
      
-     //que se visible el menu lateral h
+     //Version movil. que no sea visible el menu lateral h
      if(window.innerWidth<575){
          //caja.innerHTML=menuLH;
          menuLH.style.display="inline";
@@ -38,18 +52,6 @@
      }
  } 
 
- /*window.onscroll = function() {myFunction()};
-
- function myFunction() {
-
-     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-         //document.getElementById("myP").className = "test";
-         //console.log("hola");
-     } else {
-         //document.getElementById("myP").className = "";
-         //console.log("adios");
-     }
- } */
 
 
 
